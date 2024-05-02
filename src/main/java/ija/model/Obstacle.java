a1a035e79e45f2c5d191264e144cfeeb4fc4304f
adamvalik <xvalik05@stud.fit.vutbr.cz>
2024-05-02 15:40:07 +0200

    obstacle docs

--- src/main/java/ija/model/Obstacle.java
+++ src/main/java/ija/model/Obstacle.java
@@ -1,37 +1,53 @@
+/**
+ * @package ija.model
+ * @file Obstacle.java
+ * @class Obstacle
+ *
+ * The Obstacle class represents a static obstacle in the simulation. It is represented by a square.
+ *
+ * @author Adam Valik
+ */
+
 package ija.model;
 
-import javafx.beans.property.IntegerProperty;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Represents a static obstacle in the simulation
+ */
 public class Obstacle {
     private final double x; // [x,y] represents the top left corner of the square
     private final double y;
-    private final double side;
+    private final double side; // side length of the square
 
-    public Obstacle(double x, double y) {
-        this.x = x;
-        this.y = y;
-        this.side = 75;
-    }
-
+    /**
+     * @param x X coordinate of the obstacle
+     * @param y Y coordinate of the obstacle
+     * @param side Side length of the obstacle
+     */
     public Obstacle(double x, double y, double side) {
         this.x = x;
         this.y = y;
         this.side = side;
     }
 
+    /**
+     * @return X coordinate of the obstacle
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * @return Y coordinate of the obstacle
+     */
     public double getY() {
         return y;
     }
 
-    public double getSideLen() {
-        return side;
-    }
-
+    /**
+     * @return The representation of the obstacle as a square
+     */
     public Rectangle getRectangle() {
         return new Rectangle(x, y, side, side);
     }
